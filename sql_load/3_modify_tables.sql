c7f2bd8a281fa72d9033201092eb1034995fb685
Mehul Mahajan <mahajanmehul553@gmail.com>
2025-06-06 13:10:51 +0530

    first commit :qa!

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -24,28 +24,28 @@
  */
 -- NOTE: This has been updated from the video to fix issues with encoding
 COPY company_dim
-FROM 'C:\Users\mahaj\OneDrive\Documents\data science\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\company_dim.csv' WITH (
+FROM '[location]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY skills_dim
-FROM 'C:\Users\mahaj\OneDrive\Documents\data science\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\job_postings_fact.csv' WITH (
+FROM '[location]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY job_postings_fact
-FROM 'C:\Users\mahaj\OneDrive\Documents\data science\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\skills_dim.csv' WITH (
+FROM '[location]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
         ENCODING 'UTF8'
     );
 COPY skills_job_dim
-FROM 'C:\Users\mahaj\OneDrive\Documents\data science\SQL_PROJECT_DATA_JOB_ANALYSIS\csv_files\skills_job_dim.csv' WITH (
+FROM '[location]' WITH (
         FORMAT csv,
         HEADER true,
         DELIMITER ',',
