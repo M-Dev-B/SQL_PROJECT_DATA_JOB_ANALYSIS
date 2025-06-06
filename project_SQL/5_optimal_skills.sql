1327d09650e34388904c6e9df94a9fb6830fbbaf
Mehul Mahajan <mahajanmehul553@gmail.com>
2025-06-06 22:19:43 +0530

    changes

--- project_SQL/5_optimal_skills.sql
+++ project_SQL/5_optimal_skills.sql
@@ -1,5 +1,6 @@
 WITH top_demand AS (
-    SELECT skills_dim.skill_id,
+    SELECT 
+        skills_dim.skill_id,
         skills_dim.skills,
         COUNT(skills_job_dim.job_id) AS demand_count
     FROM job_postings_fact
@@ -26,9 +27,26 @@ SELECT top_demand.skill_id,
     demand_count,
     average_pay
 FROM top_demand
-    INNER JOIN top_pay ON top_demand.skill_id = top_pay.skill_id
-WHERE 
+INNER JOIN top_pay ON top_demand.skill_id = top_pay.skill_id
+WHERE
     demand_count > 10
 ORDER BY demand_count DESC,
     average_pay DESC
+LIMIT 25
+
+--rewritting same query in more concise way
+
+SELECT skills,
+    COUNT(*) AS demand_count,
+    ROUND(AVG(salary_year_avg), 0) AS average_pay
+FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home = TRUE
+GROUP BY skills
+HAVING
+    COUNT(*) > 10
+ORDER BY average_pay DESC
 LIMIT 25
